c02232574528d1fe8216c839c700945306ba782a
roman.kozin <romanko95.rk@gmail.com>
2016-11-27 00:52:38 +0200

    Added SMS allowed fields

--- data/rules.php
+++ data/rules.php
@@ -5,7 +5,16 @@
  */
 
 return [
-    'sms' => [],
+    'sms' => [
+        'allowed_arguments' => [
+            'id', 'translit', 'sender', 'tinyurl', 'time', 'tz',
+            'period', 'freq', 'flash', 'bin', 'push', 'hlr',
+            'ping', 'mms', 'mail', 'viber', 'call', 'voice',
+            'param', 'subj', 'charset', 'cost', 'fmt', 'list',
+            'valid', 'maxsms', 'imgcode', 'userip', 'err', 'op',
+            'pp'
+        ]
+    ],
     'mms' => [],
     'email' => [],
     'voice' => [],
